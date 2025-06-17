fa285a1bb5f8416d7a6139232f5a4b84c3306be4
Joe Kay <joelkugonza@gmail.com>
2025-06-17 11:10:54 +0300

    fix listgroup

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,19 +1,18 @@
-//{ items: [], heading: string }
+import { useState } from "react";
+
 interface Props {
   items: string[];
   heading: string;
   onSelectItem: (item: string) => void;
 }
 
-function ListGroup({items, heading, onSelectItem }: Props) {
-  const [selectedIndex, setSelectedIndex]= useState(-1);
-
-  heading = "";
+function ListGroup({ items, heading, onSelectItem }: Props) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
       <h1>{heading}</h1>
-      {items.length === 0 && <p>No item found</p>}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
@@ -23,9 +22,10 @@ function ListGroup({items, heading, onSelectItem }: Props) {
                 : "list-group-item"
             }
             key={item}
-            onClick={() => { setSelectedIndex(index); }}
-            setSelectedIndex(index);
-            onSelectItem(item);
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectItem(item);
+            }}
           >
             {item}
           </li>
